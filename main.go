14bb2184a248d72a26e0a1689990b8154ec0cf51
Bayu Fajariyanto <bayuf08@gmail.com>
2021-05-01 09:24:28 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+func main(){
+	// variabel
+	// var kalimat string = "Ini variabel string"
+	// var angka int = 4
+	// var angka float64 = 4
+	// var boolean bool = true
+	// teks := "Teks ke "
+	// batas := 10000
+
+	// if
+	// if variabel == 0 {
+	// 	fmt.Println("Variabel == ", 0)
+	// } else {
+	// 	fmt.Println("Variabel != ", 0)
+	// }
+
+	// switch
+	// switch  nomor {
+	// case 1:
+	// 	fmt.Println("Nomor == 1")
+	// case 2:
+	// 	fmt.Println("Nomor == 2")
+	// case 3:
+	// 	fmt.Println("Nomor == 3")
+	// case 4:
+	// 	fmt.Println("Nomor == 4")
+	// default:
+	// 	fmt.Println("DEFAULT")
+	// }
+
+	// for
+	// for i := 1 ; i <= batas ; i++ {
+	// 	fmt.Println(teks, i)
+	// }
+	
+	// while
+	// i := 1
+	// for i <= batas {
+	// 	fmt.Println(teks, i)
+	// 	i++
+	// }
+
+	// foreach
+	// for i, x := range teks {
+	// 	if i %2 == 0 {
+	// 		if string(x) == "a" || string(x) == "i" || string(x) == "u" || string(x) == "e" || string(x) == "o" {
+	// 			fmt.Println("index : ", i, "huruf : ", string(x))
+	// 		}
+	// 	}
+	// }
+
+	// array
+	// var array[5] bool
+	// array := [...]string{"PHP", "Golang", "Java", "JavaScript", "Python"}
+
+	// Quiz
+	// title := "Golang the best language"
+	// for i, x := range title {
+	// 	if i %2 == 0 {
+	// 		if string(x) == "a" || string(x) == "i" || string(x) == "u" || string(x) == "e" || string(x) == "o" {
+	// 			fmt.Println("index : ", i, "huruf : ", string(x))
+	// 		}
+	// 	}
+	// }
+}
